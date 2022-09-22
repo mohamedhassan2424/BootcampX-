cf637d019672dbc7c77c03dec3e3c6c84bee5b52
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-22 11:52:22 +0000

    committing code

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+    user: 'vagrant',
+    password: '123',
+    host: 'localhost',
+    database: 'bootcampx'
+  });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;`
+
+pool.query(queryString)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
